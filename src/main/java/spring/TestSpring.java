e8da9d31ca6576c444a6cf04596afe78fe6cdbe0
Solovyov Oleg <livsi8@gmail.com>
2020-11-02 12:45:32 +0200

    Lesson 7 Bean scope prototype

--- src/main/java/spring/TestSpring.java
+++ src/main/java/spring/TestSpring.java
@@ -11,10 +11,20 @@ public class TestSpring {
 //        Music music = context.getBean("classicalMusicBean", Music.class);
 //        Music music = context.getBean("rockMusicBean", Music.class);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayerBean", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayerBean", MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayerBean", MusicPlayer.class);
+        Boolean aBoolean = musicPlayer1 == musicPlayer2;
+        System.out.println("musicPlayer1 == musicPlayer2 = " + aBoolean);
+        System.out.println(musicPlayer1);
+        System.out.println(musicPlayer2);
+
+        musicPlayer1.setVolume(25);
+        System.out.println(musicPlayer1.getVolume());
+        System.out.println(musicPlayer2.getVolume());
+
+//        musicPlayer.playMusic();
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
         context.close();
     }
 }
